a04c9a18ace23733b6fc4e7c9cf1cd7ce642c3db
IlyaNad <ilyshan08@gmail.com>
2023-07-11 23:40:53 +0300

    some changes with messagebox

--- messagebox/admin.py
+++ messagebox/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-
 from .models import Message
 
-admin.site.register(Message)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "date")
+    list_filter = ("date",)
--- messagebox/models.py
+++ messagebox/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 class Message(models.Model):
     title = models.CharField(max_length=40)
     text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
+    class Meta:
+        ordering = ["-date"]
+
+    def __str__(self):
+        return self.title[:50]
